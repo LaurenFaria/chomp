995c15c5853e4f9b0d671ee631983af3546c2d6a
LaurenFaria <147637258+LaurenFaria@users.noreply.github.com>
2023-10-12 10:33:06 -0400

    Creating the screen

--- game.py
+++ game.py
@@ -1,2 +1,36 @@
-print('Hello, World')
-print ('Today is going to be a great day')
+import pygame
+import sys
+
+#Initialize oygame
+pygame.init()
+
+#screen dimensions
+screen_width = 800
+screen_height = 600
+
+#colors
+BLUE = (0,0,255)
+BROWN = (204,129,43)
+
+#create the screen
+screen = pygame.display.set_mode((screen_width, screen_height))
+pygame.display.set_caption("Blue Background with Brown Sandy Bottom")
+
+#Main loop
+
+running = True #set flag to True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    #fill screen with blue
+    screen.fill(BLUE)
+
+    #add sandy bottom
+    rectangle_height = 100
+    pygame.draw.rect(screen, BROWN, (0, screen_height - rectangle_height, screen_width, rectangle_height))
+
+    #update the display
+    pygame.display.flip()
+
+pygame.quit()
